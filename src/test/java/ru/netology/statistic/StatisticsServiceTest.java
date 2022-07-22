ba90541e4eca096ecefca4856dd3edd1e920bad9
NataBarsu <khvorostukhina.n@yandex.ru>
2022-07-22 18:59:38 +0300

    Name of test changed

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,7 +19,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxSecond() {
+    void findMaxInOtherPart() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
